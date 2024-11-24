19977e8391a33832d9985d68259430d3ebc6a8f7
Hindustanin <hindustanin@hotmail.com>
2024-11-24 10:48:25 +0700

    Старт

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,109 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Задача 1
+        System.out.println("Задача 1");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int celNakopit = 2_459_000;
+        int summa = 0;
+        int otlogeno = 15_000;
+        double procent = 1D / 100;
+        int mesyac = 1;
+
+        while (summa < celNakopit) {
+            summa += otlogeno;
+            summa = (int) (summa * (1 + procent));
+            mesyac++;
+            System.out.println("Месяц " + mesyac + ", сумма накоплений равна " + summa + " рублей");
+        }
+        // Задача 2
+        System.out.println();
+        System.out.println("Задача 2");
+
+        int nomer = 0;
+        while (nomer < 10) {
+            nomer++;
+            System.out.print(nomer + " ");
+        }
+        System.out.println();
+        for (; nomer >= 1; nomer--) {
+            System.out.print(nomer + " ");
+        }
+        // Задача 3
+        System.out.println();
+        System.out.println("Задача 3");
+
+        int naselenie = 12_000_000;
+        int rogdaemstNa1000 = 17;
+        int smertnostNa1000 = 8;
+        int god = 2024;
+        int letPoschitat = god + 10;
+        while (god < letPoschitat) {
+            naselenie += naselenie * rogdaemstNa1000 / 1000 - naselenie * smertnostNa1000 / 1000;
+            System.out.println("Год " + god + ", численность населения составляет " + naselenie);
+            god++;
+        }
+        // Задача 4
+        System.out.println();
+        System.out.println("Задача 4");
+
+        int vklad = 15_000;
+        procent = 7D / 100;
+        celNakopit = 12_000_000;
+        mesyac = 1;
+        while (vklad <= celNakopit) {
+            vklad = (int) (vklad * (1 + procent));
+            System.out.println("Месяц " + mesyac + ", сумма наколений равна " + vklad);
+            mesyac++;
+        }
+        // Задача 5
+        System.out.println();
+        System.out.println("Задача 5");
+
+        vklad = 15_000;
+        mesyac = 1;
+        while (vklad <= celNakopit) {
+            vklad = (int) (vklad * (1 + procent));
+            if (mesyac % 6 == 0) {
+                System.out.println("Месяц " + mesyac + ", сумма наколений равна " + vklad);
+            }
+            mesyac++;
+        }
+        // Задача 6
+        System.out.println();
+        System.out.println("Задача 6");
+
+        vklad = 15_000;
+        mesyac = 1;
+        int letNaVklad = 9;
+        int mesyacevNaVklad = 12 * letNaVklad;
+        while (mesyac <= mesyacevNaVklad) {
+            vklad = (int) (vklad * (1 + procent));
+            if (mesyac % 6 == 0) {
+                System.out.println("Месяц " + mesyac + ", сумма наколений равна " + vklad);
+            }
+            mesyac++;
+        }
+        // Задача 7
+        System.out.println();
+        System.out.println("Задача 7");
+
+        int chislo1Pyatnici = 1;
+        for (int den = chislo1Pyatnici; den <= 31; den+=7) {
+            System.out.println("Сегодня пятница, " + den + "-е число. Необходимо подготовить отчет");
+        }
+        // Задача 8
+        System.out.println();
+        System.out.println("Задача 8");
+
+        god = 2024;
+        int period = 79;
+        int nachalo = 0;
+        int godNachalaOtcheta = god - 200;
+        int godOkonchsniaOtcheta = god + 100;
+        for (int godKameti = nachalo; godKameti < godOkonchsniaOtcheta; godKameti += period) {
+            if (godKameti > godNachalaOtcheta) {
+                System.out.println(godKameti);
+            }
         }
     }
 }
